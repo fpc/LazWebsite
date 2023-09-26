a1b8c75ddddc6ef71270e7421bdb1c456959a7e0
Martin <laz.git@mfriebe.de>
2023-09-26 11:49:11 +0200

    (C) 2023

--- site/include/config.php
+++ site/include/config.php
@@ -28,7 +28,7 @@ $error_webmaster_email = 'lazarus@freepascal.org';
 $home_php_name = 'index.php';
 
 
-$copyrightyear = '2022';
+$copyrightyear = '2023';
 
 //------ Download Information (Link/version/mirror...) -----//
 //--- versions ---
